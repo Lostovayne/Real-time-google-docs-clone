61ae5b0905da662cfd26e830bf1d63c68cde6f17
Lostovayne <moises.barrera.dev@gmail.com>
2025-01-02 03:21:45 -0300

    feat: update editor functionality and configuration
    - Added new OpenNext configuration file.
    - Updated .gitignore to include new files.
    - Modified next.config.ts for improved TypeScript and ESLint settings.
    - Enhanced package.json with new scripts and updated dependencies.
    - Updated wrangler.toml for new worker entry point and compatibility date.
    - Added VSCode settings for TypeScript.
    - Refactored editor and toolbar components to improve functionality, including new alignment and list buttons, and enhanced text styling options.
    - Improved overall code structure and consistency.

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,96 +1,100 @@
-'use client';
+"use client";
 
-import Code from '@tiptap/extension-code';
-import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight';
-import { Color } from '@tiptap/extension-color';
-import FontFamily from '@tiptap/extension-font-family';
-import Heading from '@tiptap/extension-heading';
-import Highlight from '@tiptap/extension-highlight';
-import Image from '@tiptap/extension-image';
-import Link from '@tiptap/extension-link';
-import Table from '@tiptap/extension-table';
-import TableCell from '@tiptap/extension-table-cell';
-import TableHeader from '@tiptap/extension-table-header';
-import TableRow from '@tiptap/extension-table-row';
-import TaskItem from '@tiptap/extension-task-item';
-import TaskList from '@tiptap/extension-task-list';
-import TextStyle from '@tiptap/extension-text-style';
-import Underline from '@tiptap/extension-underline';
-import StarterKit from '@tiptap/starter-kit';
-import ImageResize from 'tiptap-extension-resize-image';
+import Code from "@tiptap/extension-code";
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import { Color } from "@tiptap/extension-color";
+import FontFamily from "@tiptap/extension-font-family";
+import Heading from "@tiptap/extension-heading";
+import Highlight from "@tiptap/extension-highlight";
+import Image from "@tiptap/extension-image";
+import Link from "@tiptap/extension-link";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import TextStyle from "@tiptap/extension-text-style";
+import Underline from "@tiptap/extension-underline";
+import StarterKit from "@tiptap/starter-kit";
+import ImageResize from "tiptap-extension-resize-image";
+import TextAlign from "@tiptap/extension-text-align";
 
-import { useEditorStore } from '@/store/use-editor-store';
-import { EditorContent, useEditor } from '@tiptap/react';
-import { common, createLowlight } from 'lowlight';
+import { useEditorStore } from "@/store/use-editor-store";
+import { EditorContent, useEditor } from "@tiptap/react";
+import { common, createLowlight } from "lowlight";
 
 const Editor = () => {
-  const { setEditor } = useEditorStore();
-  const lowlight = createLowlight();
-  lowlight.register(common);
+	const { setEditor } = useEditorStore();
+	const lowlight = createLowlight();
+	lowlight.register(common);
 
-  const editor = useEditor({
-    onCreate: ({ editor }) => {
-      setEditor(editor);
-    },
-    onDestroy: () => {
-      setEditor(null);
-    },
-    onUpdate({ editor }) {
-      setEditor(editor);
-    },
-    onSelectionUpdate({ editor }) {
-      setEditor(editor);
-    },
-    onTransaction({ editor }) {
-      setEditor(editor);
-    },
-    onFocus({ editor }) {
-      setEditor(editor);
-    },
-    onBlur({ editor }) {
-      setEditor(editor);
-    },
-    onContentError({ editor }) {
-      setEditor(editor);
-    },
+	const editor = useEditor({
+		onCreate: ({ editor }) => {
+			setEditor(editor);
+		},
+		onDestroy: () => {
+			setEditor(null);
+		},
+		onUpdate({ editor }) {
+			setEditor(editor);
+		},
+		onSelectionUpdate({ editor }) {
+			setEditor(editor);
+		},
+		onTransaction({ editor }) {
+			setEditor(editor);
+		},
+		onFocus({ editor }) {
+			setEditor(editor);
+		},
+		onBlur({ editor }) {
+			setEditor(editor);
+		},
+		onContentError({ editor }) {
+			setEditor(editor);
+		},
 
-    editorProps: {
-      attributes: {
-        style: 'padding-left: 56px; padding-right:56px;',
-        class:
-          'focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text',
-      },
-    },
-    extensions: [
-      StarterKit,
-      Heading,
-      FontFamily,
-      Color,
-      Highlight.configure({
-        multicolor: true,
-      }),
-      CodeBlockLowlight.configure({
-        lowlight,
-        defaultLanguage: 'javascript',
-      }),
-      Link.configure({
-        openOnClick: false,
-        autolink: true,
-        defaultProtocol: 'https',
-      }),
-      TextStyle,
-      Code,
-      ImageResize,
-      Image,
-      Table,
-      TableCell,
-      TableHeader,
-      TableRow,
-      TaskItem.configure({ nested: true }),
-      TaskList,
-      Underline,
-    ],
-    content: `
+		editorProps: {
+			attributes: {
+				style: "padding-left: 56px; padding-right:56px;",
+				class:
+					"focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
+			}
+		},
+		extensions: [
+			StarterKit,
+			Heading,
+			FontFamily,
+			Color,
+			TextAlign.configure({
+				types: ["heading", "paragraph"]
+			}),
+			Highlight.configure({
+				multicolor: true
+			}),
+			CodeBlockLowlight.configure({
+				lowlight,
+				defaultLanguage: "javascript"
+			}),
+			Link.configure({
+				openOnClick: false,
+				autolink: true,
+				defaultProtocol: "https"
+			}),
+			TextStyle,
+			Code,
+			ImageResize,
+			Image,
+			Table,
+			TableCell,
+			TableHeader,
+			TableRow,
+			TaskItem.configure({ nested: true }),
+			TaskList,
+			Underline
+		],
+		content: `
         <h1 className="text-6xl font-bold">Google Docs Editor </h1>
 
         <p>
@@ -134,16 +138,16 @@ const Editor = () => {
 
 
       `,
-    immediatelyRender: false,
-  });
+		immediatelyRender: false
+	});
 
-  return (
-    <div className='size-full overflow-x-auto bg-[#F9FBFD] min-h-screen px-4 print:p-0 print:bg-white print:overflow-visible'>
-      <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
-        <EditorContent editor={editor} />
-      </div>
-    </div>
-  );
+	return (
+		<div className="size-full overflow-x-auto bg-[#F9FBFD] min-h-screen px-4 print:p-0 print:bg-white print:overflow-visible">
+			<div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
+				<EditorContent editor={editor} />
+			</div>
+		</div>
+	);
 };
 
 export default Editor;
